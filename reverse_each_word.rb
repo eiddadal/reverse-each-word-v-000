3899c8beaa01fcd8624c6d46895b2211b3cd6ae5
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-27 13:30:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
-    array =string.split(" ")
-    string.collect do |array|
-      array.reverse! 
-    end 
-    array.join(" ")
-    
-end
+  array= string.split(" ")
+  array.collect do |word|
+    word.reverse! 
+  end 
+  array.join(" ")
+end 
