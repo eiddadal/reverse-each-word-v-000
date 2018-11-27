dc441e08ad28e64864c5fe33240233cb6b2c03d9
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-27 13:21:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word
     words="Hello there, and who are you?".split(/\s+/)
-    words.collect{|w|wl=w.length-1;(0..wl).map{|i|w[wl-i]}.join}.join(' ')
+    words.collect{|w|wl=w.length-1;(0..wl).collect{|i|w[wl-i]}.join}.join(' ')
 end
