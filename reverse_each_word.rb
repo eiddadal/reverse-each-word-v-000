13fb5e78de6f8883c366490cf0ee220b9cfb4e29
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-27 13:20:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word
-array ="Hello there, and who are you?".split (" ") 
-array.reverse!
-end 
-
-  def reverse_words
     "Hello there, and who are you?".split(/\s+/).map{|w|wl=w.length-1;(0..wl).map{|i|w[wl-i]}.join}.join(' ')
   end
